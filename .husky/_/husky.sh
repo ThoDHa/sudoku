b92662c66e5fead9054ef4d474eabb6f08a19071
Tho Ha <ThoDHa@users.noreply.github.com>
2026-07-13 10:00:10 -0400

    chore: adopt TypeScript 7, align deps, add Docker Dependabot
    Upgrades TypeScript 6 to 7 (zero breakage), aligns @types/node to 24
    matching the runtime, adds engines.node >= 24, adds a docker ecosystem
    to Dependabot for base-image tracking, and clears minor/patch lag
    across npm and Go dependencies.

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,30 +1,9 @@
-#!/bin/sh
-if [ -z "$husky_skip_init" ]; then
-  debug() {
-    if [ "$HUSKY_DEBUG" = "1" ]; then
-      echo "husky (debug) - $1"
-    fi
-  }
+echo "husky - DEPRECATED
 
-  readonly hook_name="$(basename "$0")"
-  debug "starting $hook_name..."
+Please remove the following two lines from $0:
 
-  if [ "$HUSKY" = "0" ]; then
-    debug "HUSKY=0, skip hook"
-    exit 0
-  fi
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
 
-  export readonly husky_skip_init=1
-  sh -e "$0" "$@"
-  exitCode="$?"
-
-  if [ $exitCode != 0 ]; then
-    echo "husky - $hook_name hook exited with code $exitCode (error)"
-  fi
-
-  if [ $exitCode = 127 ]; then
-    echo "husky - command not found in PATH=$PATH"
-  fi
-
-  exit $exitCode
-fi
+They WILL FAIL in v10.0.0
+"
